824380808c46506313d43125acebe77562299b29
simssousa15 <sbesousa@gmail.com>
2025-01-03 14:25:21 +0100

    basic shared_ptr

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -11,23 +11,25 @@ class shared_ptr{
 
         void clean_up(){
             if(cnt != nullptr){
-                *cnt--;
+                (*cnt)--;
 
-                if(*cnt == 0){
-                    delete cnt;
-                    delete ptr;
+                if((*cnt) == 0){
+                    if(cnt){ delete cnt; }
+                    if(ptr){ delete ptr; }
+                    cnt = nullptr;
+                    ptr = nullptr;
                 }
             }
         }
 
     public:
         // constructors
-        shared_ptr() = delete;
+        shared_ptr(): ptr(nullptr), cnt(nullptr){};
         shared_ptr(T val): ptr(new T(val)), cnt(new uint(1)){}
-        // shared_ptr(T* val): ptr(val), cnt(new uint(1)){}
+        shared_ptr(T* val): ptr(val), cnt(new uint(1)){}
 
         // overloads
-        T* operator->() const { return this->ptr; }
+        T* operator->() const { return ptr; }
         T& operator*() const{ return *ptr; }
 
         // destructor
@@ -38,7 +40,7 @@ class shared_ptr{
             ptr = s.ptr;
             cnt = s.cnt;
 
-            if(ptr != nullptr){ *cnt++; }
+            if(cnt != nullptr){ (*cnt)++; }
         }
 
         // copy assignment
@@ -47,7 +49,7 @@ class shared_ptr{
             ptr = s.ptr;
             cnt = s.cnt;
 
-            if(ptr != nullptr){ *cnt++; }
+            if(cnt != nullptr){ (*cnt)++; }
 
             return *this;
         }
@@ -61,12 +63,12 @@ class shared_ptr{
         }
 
         // move assignment
-        shared_ptr& operator=(const shared_ptr&& s){
+        shared_ptr& operator=(shared_ptr&& s){
             clean_up();
             cnt = s.cnt;
             ptr = s.ptr;
-            s.ptr = nullptr;
-            s.cnt = nullptr;
+            s.cnt = cnt;
+            s.ptr = ptr;
 
             return *this;
         }
@@ -74,7 +76,37 @@ class shared_ptr{
 };
 
 int main(){
-    shared_ptr<int*> ptr1(new int);
-    auto ptr2 = ptr1;
+    {
+        shared_ptr<int> ptr1; 
+        std::cout << "Default constructor ok." << std::endl;
+
+        shared_ptr<int> ptr2(new int(10));
+        std::cout << "Param constructor ok." << std::endl;
+
+        shared_ptr<int> ptr3(ptr2);
+        std::cout << "Copy constructor ok." << std::endl;
+
+        ptr3 = ptr2;
+        std::cout << "Copy assignment ok." << std::endl;
+
+        shared_ptr<int> ptr4(std::move(ptr1));
+        std::cout << "Move constructor ok" << std:: endl;
+
+        ptr2 = std::move(ptr3);
+        std::cout << "Move assignment ok" << std:: endl;
+
+        std::cout << "Pointer value: " << (*ptr2) << std::endl;
+    }
+
+    
+    std::cout << "Destructor ok." << std::endl;
+
     return 0;
 }
+
+// g++ -o a.out shared_ptr.cpp && ./a.out
+
+/* std::cout << "this->ptr " << this->ptr << std::endl;
+std::cout << "this->cnt " << this->cnt << std::endl;
+std::cout << "other->ptr " << s.ptr << std::endl;
+std::cout << "other->cnt " << s.cnt << std::endl; */
